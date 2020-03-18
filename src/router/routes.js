984b2be188b8ad2fdd9ec5956b2f8c1c31ea558d
fredrik berntsson <fredrikberntsson@hotmail.com>
2020-03-18 16:34:54 +0100

    made Navigation overlay

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,31 +3,36 @@ import About from "@/views/About";
 import Menu from "@/views/Menu";
 import Cart from "@/views/Cart";
 import Status from "@/views/Status";
+import Nav from "@/components/Navigation";
 
-export default [
-  {
-    path: "/landing",
-    name: "Landing",
-    component: Landing
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: About
-  },
-  {
-    path: "/menu",
-    name: "Menu",
-    component: Menu
-  },
-  {
-    path: "/cart",
-    name: "Cart",
-    component: Cart
-  },
-  {
-    path: "/status",
-    name: "Status",
-    component: Status
-  }
+export default [{
+        path: "/landing",
+        name: "Landing",
+        component: Landing
+    },
+    {
+        path: "/about",
+        name: "About",
+        component: About
+    },
+    {
+        path: "/menu",
+        name: "Menu",
+        component: Menu
+    },
+    {
+        path: "/cart",
+        name: "Cart",
+        component: Cart
+    },
+    {
+        path: "/status",
+        name: "Status",
+        component: Status
+    },
+    {
+        path: "/nav",
+        name: "Nav",
+        component: Nav
+    }
 ];
