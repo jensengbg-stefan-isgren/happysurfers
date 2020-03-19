17ea76a5a11816b5bc5a4b53001f779c81bed247
fredrik berntsson <fredrikberntsson@hotmail.com>
2020-03-19 15:39:11 +0100

    added landing page

---
+++ src/views/Landing.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="landing" @click="toMenu">
+    <img src="../assets/graphics/airbean-landing.svg" alt />
+  </section>
+</template>
+
+<script>
+export default {
+  name: "landing",
+  methods: {
+    toMenu() {
+      this.$router.push("/menu");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.landing {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: url("../assets/graphics/intro-graphic-left.svg") center left
+      no-repeat,
+    url("../assets/graphics/intro-graphic-right.svg") center right no-repeat;
+  background-color: $dark-green;
+  background-size: contain;
+}
+</style>
