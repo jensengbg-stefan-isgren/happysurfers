4287f364776f73671e1a02a932a54599ee76b50b
Stefan Isgren <stefan.isgren@gmail.com>
2020-03-31 13:28:26 +0200

    fixed some logic for addStamps
    fixed logic for addstamps method to check that there is a user created

--- server/routes/beans.js
+++ server/routes/beans.js
@@ -2,6 +2,9 @@ const { Router } = require("express");
 const router = new Router();
 const beansController = require("../controllers/beansController");
 
-router.route("/").get(beansController.getProducts);
+router
+  .route("/")
+  .get(beansController.getProducts)
+
 router.route("/key").get(beansController.getKey);
 module.exports = router;
